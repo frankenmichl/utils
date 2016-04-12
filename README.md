b48f65fd212420c0151906430188cbec5bc466fe
Michael Moese <michael.moese@men.de>
2016-04-12 10:32:44 +0200

    initial commit: Added gitinfo.sh and README.md
    Signed-off-by: Michael Moese <michael.moese@men.de>

---
+++ README.md
@@ -0,0 +1,4 @@
+Random collection of useful helper scripts
+
+gitinfo.sh:
+recursively finds git repositories and displays current branch and state
---
+++ gitinfo.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+RESULTS=""
+for i in `find . -name ".git"`;
+do
+	pushd . > /dev/null
+	REPO=${i%/.git}
+	cd $REPO
+	BRANCH=`git branch | grep "^*" | cut -d " " -f 2`
+	STATUS=""
+	if [[ -z $(git status -s) ]]; then
+		STATUS="clean"
+	else
+		STATUS="dirty"
+	fi
+	RESULTS="$RESULTS\n$REPO:$BRANCH:$STATUS"
+	popd > /dev/null
+done
+echo -e $RESULTS | column -t -s ':'
